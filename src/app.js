367b05e2c5cfc64a3c7903e4cffadc29c7444d18
BinyaminYacovson <bj57577@gmail.com>
2024-10-07 15:39:11 +0300

    Update server startup message to include a hyphen instead of a colon

--- src/app.js
+++ src/app.js
@@ -12,4 +12,4 @@ app.use('/auth', require('./routes/authRouter'))
 app.use('/user', require('./routes/userRouter'))
 app.use('/greenEye', require('./routes/greenEyeRouter'))
 
-app.listen(port, () => console.log("Server is up and runing on port: " + port))
+app.listen(port, () => console.log("Server is up and runing on port-" + port))
