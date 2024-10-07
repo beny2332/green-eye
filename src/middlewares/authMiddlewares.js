a3f45a5585ff6059f9ec1921a16ed6af12fe3e21
BinyaminYacovson <bj57577@gmail.com>
2024-10-07 11:42:20 +0300

    empty middlewares

---
+++ src/middlewares/authMiddlewares.js
@@ -0,0 +1,20 @@
+const onlyCommanders = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+const onlySoldiersAndCommanders = async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+module.exports = {
+    onlyCommanders,
+    onlySoldiersAndCommanders
+}
